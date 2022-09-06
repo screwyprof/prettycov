5ce4486675cae617508db87adfc4c553b75be7a8
Maksim Shcherbo <max@happygopher.nl>
2022-09-06 22:15:03 +0100

    Fix version

--- cmd/prettycov/prettycov.go
+++ cmd/prettycov/prettycov.go
@@ -67,11 +67,9 @@ func showUsage() {
 
 func showVersion() {
 	// When app is being installed using `go install prettycov@v1.2.3`, the ldflags won't be passed
-	// and the version will be empty.
-	//
-	// Populate version using specified project tag
-	if info, ok := debug.ReadBuildInfo(); ok {
-		if version == "" {
+	// and the version will be empty. In this case, we try to populate version using build info.
+	if version == "" {
+		if info, ok := debug.ReadBuildInfo(); ok {
 			version = info.Main.Version
 		}
 	}
